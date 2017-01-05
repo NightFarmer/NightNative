460897358b657229d0fd5f79f210c1ba157e64ce
zhangfan <494090597@qq.com>
2017-01-05 18:19:01 +0800

    初步完成九宫格的拖拽排序功能

--- mytest/testScrollView2.js
+++ mytest/testScrollView2.js
@@ -6,6 +6,7 @@ import {
     TouchableHighlight,
     ToastAndroid,
     Platform,
+    Animated,
 } from 'react-native';
 
 
@@ -26,12 +27,26 @@ class TestScrollView extends Component {
     constructor(props) {
         super(props);
         let initList = []
-        for (let i = 20; i > 0; i--) {
+        for (let i = 0; i < 20; i++) {
             initList.push(i)
         }
+        let boxSize = Dimensions.get('window').width / 3
+        let eleList = initList.map((v, i) => {
+            console.info(v, i)
+            return {
+                data: v,
+                marginLeft: new Animated.Value(i % 3 * boxSize),
+                marginTop: new Animated.Value(Math.floor(i / 3) * boxSize),
+                zIndex: 0,
+                index: i
+            }
+        })
         this.state = {
             num: 10,
-            dataList: initList,
+            dataList: eleList,
+            scrollEnabled: true,
+            bounceValue: new Animated.Value(0),
+            pan: new Animated.ValueXY()
             // justifyContent: 'flex-start'
         }
         // UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
@@ -66,27 +81,188 @@ class TestScrollView extends Component {
     //         </View>
     //     );
     // }
+    maxzIndex = 1;
+
+    // component
+
+    componentDidMount() {
+        // this.state.bounceValue.setValue(1.5);     // 设置一个较大的初始值
+        // Animated.spring(                          // 可选的基本动画类型: spring, decay, timing
+        //     this.state.bounceValue,                 // 将`bounceValue`值动画化
+        //     {
+        //         toValue: 0.8,                         // 将其值以动画的形式改到一个较小值
+        //         friction: 1,                          // Bouncier spring
+        //     }
+        // ).start();                                // 开始执行动画
+
+
+    }
 
     render() {
-        let items = []
         let boxSize = Dimensions.get('window').width / 3
-        console.info(boxSize)
-        for (let i = 0; i < this.state.dataList.length; i++) {
-            items.push(
-                <Text key={i} style={[myStyles.box, {
-                    position: 'absolute',
-                    marginLeft: i % 3 * boxSize,
-                    marginTop: Math.floor(i / 3) * boxSize
-                }]} >
-                    {this.state.dataList[i]}
-                </Text>
+        let items = []
+        items = this.state.dataList.map((d) => {
+            return (
+                <Animated.View
+                    style={[myStyles.box, {
+                        position: 'absolute',
+                        // marginLeft: d.marginLeft,
+                        // marginTop: d.marginTop,
+                        zIndex: d.zIndex,
+                        // transform: [                        // `transform`是一个有序数组（动画按顺序执行）
+                        // { scale: this.state.bounceValue },  // 将`bounceValue`赋值给 `scale`
+                        // { translateX: this.state.bounceValue }
+                        // ],
+                        marginLeft: d.marginLeft,
+                        marginTop: d.marginTop
+                    }]}
+
+                    key={d.data}
+                    onStartShouldSetResponder={(evt) => {
+                        console.info("onStartShouldSetResponder")
+                        return true
+                    } }
+                    onResponderGrant={(evt) => {
+                        console.info('onResponderGrant')
+                        d.zIndex = this.maxzIndex++
+                        this.setState({ scrollEnabled: false });
+                    } }
+                    onResponderMove={(evt) => {
+                        // console.info("onResponderMove " + evt.nativeEvent.pageX + " " + evt.nativeEvent.locationX)
+                        let newLeft = evt.nativeEvent.pageX - evt.nativeEvent.locationX
+                        let newTop = evt.nativeEvent.pageY - evt.nativeEvent.locationY
+
+                        // let selfIndex = 0;
+                        // for (let i = 0; i < this.state.dataList.length; i++) {
+                        //     console.info(this.state.dataList[i].data, d.data, this.state.dataList[i].data == d.data)
+                        //     if (this.state.dataList[i].data == d.data) {
+                        //         selfIndex = i;
+                        //         console.info(i)
+                        //         break
+                        //     }
+                        // }
+                        // let selfIndex = this.state.dataList.indexOf(d)
+                        let selfIndex = d.index
+                        let currentIndex = Math.floor((newTop + boxSize / 2) / boxSize) * 3 + Math.floor((newLeft + boxSize / 2) / boxSize)
+                        console.info(selfIndex, currentIndex)
+                        d.marginLeft.setValue(newLeft)
+                        d.marginTop.setValue(newTop)
+                        let dataList = this.state.dataList
+                        let findNodeByIndex = function (index) {
+                            for (let i = 0; i < dataList.length; i++) {
+                                if (dataList[i].index == index) {
+                                    return dataList[i]
+                                }
+                            }
+                        }
+                        if (currentIndex > selfIndex) {
+                            for (let i = selfIndex + 1; i <= currentIndex; i++) {
+                                let newIndex = i - 1;
+                                // let newIndex = dataList[i].index - 1
+                                let moveNode = findNodeByIndex(i)
+                                moveNode.index = newIndex
+                                Animated.timing(                          // 可选的基本动画类型: spring, decay, timing
+                                    moveNode.marginLeft,                 // 将`bounceValue`值动画化
+                                    {
+                                        toValue: newIndex % 3 * boxSize,                         // 将其值以动画的形式改到一个较小值
+                                        duration: 250
+                                    }
+                                ).start();                                // 开始执行动画
+                                Animated.timing(                          // 可选的基本动画类型: spring, decay, timing
+                                    moveNode.marginTop,                 // 将`bounceValue`值动画化
+                                    {
+                                        toValue: Math.floor(newIndex / 3) * boxSize,                         // 将其值以动画的形式改到一个较小值
+                                        duration: 250
+                                    }
+                                ).start();                                // 开始执行动画
+                                // moveNode.marginLeft = newIndex % 3 * boxSize;
+                                // moveNode.marginTop = Math.floor(newIndex / 3) * boxSize;
+                            }
+                            d.index = currentIndex
+                        }
+                        if (currentIndex < selfIndex) {
+                            for (let i = selfIndex - 1; i >= currentIndex; i--) {
+                                let newIndex = i + 1;
+                                // let newIndex = dataList[i].index + 1
+                                let moveNode = findNodeByIndex(i)
+                                moveNode.index = newIndex
+
+                                // this.state.bounceValue.setValue(1.5);     // 设置一个较大的初始值
+                                Animated.timing(                          // 可选的基本动画类型: spring, decay, timing
+                                    moveNode.marginLeft,                 // 将`bounceValue`值动画化
+                                    {
+                                        toValue: newIndex % 3 * boxSize,                         // 将其值以动画的形式改到一个较小值
+                                        duration: 250
+                                    }
+                                ).start();                                // 开始执行动画
+                                Animated.timing(                          // 可选的基本动画类型: spring, decay, timing
+                                    moveNode.marginTop,                 // 将`bounceValue`值动画化
+                                    {
+                                        toValue: Math.floor(newIndex / 3) * boxSize,                         // 将其值以动画的形式改到一个较小值
+                                        duration: 250
+                                    }
+                                ).start();                                // 开始执行动画
+
+
+                                // Animated.timing(
+                                //     moveNode.marginLeft,
+                                //     { toValue: newIndex % 3 * boxSize }
+                                // ).start
+                                // moveNode.marginLeft = newIndex % 3 * boxSize;
+                                // moveNode.marginTop = Math.floor(newIndex / 3) * boxSize;
+                            }
+                            d.index = currentIndex
+                        }
+                        // this.state.dataList = this.state.
+                        // this.state.dataList.forEach(function (element) {
+                        // console.info(element.data, element.index)
+                        // }, this);
+                        this.setState({ dataList: dataList });
+                    } }
+                    onResponderRelease={(evt) => {
+                        console.info('onResponderRelease')
+                        this.setState({ scrollEnabled: true });
+
+                        let newIndex = d.index                                // this.state.bounceValue.setValue(1.5);     // 设置一个较大的初始值
+                        Animated.timing(                          // 可选的基本动画类型: spring, decay, timing
+                            d.marginLeft,                 // 将`bounceValue`值动画化
+                            {
+                                toValue: newIndex % 3 * boxSize,                         // 将其值以动画的形式改到一个较小值
+                                duration: 200
+                            }
+                        ).start();                                // 开始执行动画
+                        Animated.timing(                          // 可选的基本动画类型: spring, decay, timing
+                            d.marginTop,                 // 将`bounceValue`值动画化
+                            {
+                                toValue: Math.floor(newIndex / 3) * boxSize,                         // 将其值以动画的形式改到一个较小值
+                                duration: 300
+                            }
+                        ).start();                                // 开始执行动画
+                    } }
+                    onResponderTerminationRequest={(evt) => {
+                        console.info('onResponderTerminationRequest')
+                        return true
+                    } }
+                    onResponderTerminate={(evt) => {
+                        console.info('onResponderTerminate')
+                    } }
+                    >
+
+                    <Text key={d.data} style={myStyles.text} >
+                        {d.data} - {d.index}
+                    </Text>
+                </Animated.View>
             )
-        }
+        })
         let listHeight = Math.ceil(this.state.dataList.length / 3) * boxSize
 
         //也可以在循环的地方使用list的map方法来进行映射，得到组件的list
         return (
             <ScrollView
+                scrollEnabled={this.state.scrollEnabled}
+                // onScroll={(x) => {
+                //     console.info(x)
+                // } }
                 >
                 <View
                     style={{
@@ -104,12 +280,14 @@ class TestScrollView extends Component {
 }
 
 const myStyles = StyleSheet.create({
-    box: {
-
-        width: Dimensions.get('window').width / 3,
-        height: Dimensions.get('window').width / 3,
+    text: {
         color: "#ff0000",
         fontSize: 30
+    },
+    box: {
+        width: Dimensions.get('window').width / 3,
+        height: Dimensions.get('window').width / 3,
+        backgroundColor: '#99ff9988',
     }
 })
 
