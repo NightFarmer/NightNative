c535f35b79f463f030dde9f4b6a770260781a6bf
zhangfan <494090597@qq.com>
2017-01-06 11:39:10 +0800

    解决item点击事件和拖拽手势冲突

--- common/MainFunctionGrid.js
+++ common/MainFunctionGrid.js
@@ -8,6 +8,7 @@ import {
     TouchableHighlight,
     Animated,
     PanResponder,
+    TouchableOpacity,
 } from 'react-native';
 
 let boxSize = Dimensions.get('window').width / 3
@@ -32,7 +33,8 @@ export default class MainFunctionGrid extends Component {
                 moveX: 0,
                 moveY: 0,
                 zIndex: 0,
-                index: i
+                index: i,
+                onClick: this.onItemClick
             }
         })
         this.state = {
@@ -41,8 +43,9 @@ export default class MainFunctionGrid extends Component {
         }
     }
 
-    evtY = 0;
-    evtX = 0;
+    onItemClick(node) {
+        console.info(node.data)
+    }
 
     render() {
         return (
@@ -74,68 +77,32 @@ export default class MainFunctionGrid extends Component {
                         marginLeft: d.marginLeft,
                         marginTop: d.marginTop
                     }]}
-
                     key={d.data}
                     {...this.newPanResponder(d).panHandlers}
-
-                    // onStartShouldSetResponder={(evt) => {
-                    //     console.info("onStartShouldSetResponder")
-                    //     return true
-                    // } }
-                    // onResponderGrant={(evt) => {
-                    //     console.info('onResponderGrant')
-                    //     d.zIndex = this.maxzIndex++
-                    //     this.setState({ scrollEnabled: false });
-                    // } }
-
-                    // onResponderMove={(evt) => {
-                    //     console.info("onResponderMove " + evt.nativeEvent.pageY + " " + evt.nativeEvent.locationY + " " + this.evtY)
-                    //     let newLeft = evt.nativeEvent.pageX - evt.nativeEvent.locationX
-                    //     let newTop = evt.nativeEvent.pageY - evt.nativeEvent.locationY
-                    //     d.marginLeft.setValue(newLeft)
-                    //     d.marginTop.setValue(newTop)
-
-                    //     let selfIndex = d.index
-                    //     let currentIndex = Math.floor((newTop + boxSize / 2) / boxSize) * 3 + Math.floor((newLeft + boxSize / 2) / boxSize)
-                    //     // console.info(selfIndex, currentIndex)
-
-                    //     if (currentIndex > selfIndex) {
-                    //         for (let i = selfIndex + 1; i <= currentIndex; i++) {
-                    //             let newIndex = i - 1;
-                    //             let moveNode = this.findNodeByIndex(i)
-                    //             moveNode.index = newIndex
-                    //             this._setMarginWithAnim(moveNode, 250)
-                    //         }
-                    //         d.index = currentIndex
-                    //     }
-                    //     if (currentIndex < selfIndex) {
-                    //         for (let i = selfIndex - 1; i >= currentIndex; i--) {
-                    //             let newIndex = i + 1;
-                    //             let moveNode = this.findNodeByIndex(i)
-                    //             moveNode.index = newIndex
-                    //             this._setMarginWithAnim(moveNode, 250)
-                    //         }
-                    //         d.index = currentIndex
-                    //     }
-                    //     this.setState({});
-                    // } }
-                    // onResponderRelease={(evt) => {
-                    //     console.info('onResponderRelease')
-                    //     this.setState({ scrollEnabled: true });
-                    //     this._setMarginWithAnim(d, 200)
-                    // } }
-                    // onResponderTerminationRequest={(evt) => {
-                    //     console.info('onResponderTerminationRequest')
-                    //     return true
-                    // } }
-                    // onResponderTerminate={(evt) => {
-                    //     console.info('onResponderTerminate')
-                    // } }
                     >
 
-                    <Text key={d.data} style={myStyles.text} >
-                        {d.data} - {d.index}
-                    </Text>
+                    <TouchableOpacity
+                        style={{
+                            flex: 1,
+                            justifyContent: 'center'
+                        }}
+                        onPress={() => {
+                            console.info('onclick..')
+                        } } >
+                        <View
+                            style={{
+                                flex: 1,
+                                backgroundColor: "#AAFF0088",
+                                alignSelf: 'stretch',
+                                alignItems: 'center',
+                                justifyContent: 'center'
+                            }}
+                            >
+                            <Text key={d.data} style={myStyles.text} >
+                                {d.data} - {d.index}
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
                 </Animated.View>
             )
         })
@@ -175,33 +142,47 @@ export default class MainFunctionGrid extends Component {
         return PanResponder.create({
             // 要求成为响应者：
             onStartShouldSetPanResponder: (evt, gestureState) => {
+                // console.info('onStartShouldSetPanResponder')
                 if (gestureState.numberActiveTouches <= 1) {
                     node.touchTimer = setTimeout(() => {
-                        node.touchable = true
+                        node.moveable = true
                         node.zIndex = this.maxzIndex++
                         this.setState({ scrollEnabled: false });
                     }, 700);
                 }
                 return false
             },
-            // onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+            onStartShouldSetPanResponderCapture: (evt, gestureState) => {
+                // console.info('onStartShouldSetPanResponderCapture')
+                if (gestureState.numberActiveTouches <= 1) {
+                    node.touchTimer = setTimeout(() => {
+                        node.moveable = true
+                        node.zIndex = this.maxzIndex++
+                        this.setState({ scrollEnabled: false });
+                    }, 700);
+                }
+                return false
+            },
             onMoveShouldSetPanResponder: (evt, gestureState) => {
+                // console.info('onMoveShouldSetPanResponder')
                 if (node.touchTimer) {
                     clearTimeout(node.touchTimer)
+                    this.setState({ scrollEnabled: true });
                 }
-                return node.touchable
+                return node.moveable
             },
             onMoveShouldSetPanResponderCapture: (evt, gestureState) => {
-
+                // console.info('onMoveShouldSetPanResponderCapture')
+                if (node.touchTimer) {
+                    clearTimeout(node.touchTimer)
+                    this.setState({ scrollEnabled: true });
+                }
+                return node.moveable
             },
 
             onPanResponderGrant: (evt, gestureState) => {
                 // 开始手势操作。给用户一些视觉反馈，让他们知道发生了什么事情！
-
                 // gestureState.{x,y}0 现在会被设置为0
-
-
-
             },
             onPanResponderMove: (evt, gestureState) => {
                 // 最近一次的移动距离为gestureState.move{X,Y}
@@ -209,7 +190,7 @@ export default class MainFunctionGrid extends Component {
                 // 从成为响应者开始时的累计手势移动距离为gestureState.d{x,y}
 
                 // console.info("onResponderMove " + evt.nativeEvent.pageY + " " + evt.nativeEvent.locationY + " " + this.evtY)
-                console.info("onPanResponderMove" + gestureState.y0)
+                // console.info("onPanResponderMove" + gestureState.y0)
                 // let newLeft = evt.nativeEvent.pageX - evt.nativeEvent.locationX
                 // let newTop = evt.nativeEvent.pageY - evt.nativeEvent.locationY
                 node.moveX += gestureState.dx;
@@ -224,7 +205,7 @@ export default class MainFunctionGrid extends Component {
                 let currentIndex = Math.floor((newTop + boxSize / 2) / boxSize) * 3 + Math.floor((newLeft + boxSize / 2) / boxSize)
                 // console.info(selfIndex, currentIndex)
 
-                if (currentIndex > selfIndex) {
+                if (currentIndex > selfIndex && currentIndex < this.state.dataList.length) {
                     for (let i = selfIndex + 1; i <= currentIndex; i++) {
                         let newIndex = i - 1;
                         let moveNode = this.findNodeByIndex(i)
@@ -233,7 +214,7 @@ export default class MainFunctionGrid extends Component {
                     }
                     node.index = currentIndex
                 }
-                if (currentIndex < selfIndex) {
+                if (currentIndex < selfIndex && currentIndex >= 0) {
                     for (let i = selfIndex - 1; i >= currentIndex; i--) {
                         let newIndex = i + 1;
                         let moveNode = this.findNodeByIndex(i)
@@ -248,7 +229,12 @@ export default class MainFunctionGrid extends Component {
             onPanResponderRelease: (evt, gestureState) => {
                 // 用户放开了所有的触摸点，且此时视图已经成为了响应者。
                 // 一般来说这意味着一个手势操作已经成功完成。
-                node.touchable = false
+                // console.info(node.touchable)
+                // if (!node.touchable) {
+                //     console.info(2333)
+                //     node.onClick(node)
+                // }
+                node.moveable = false
                 if (node.touchTimer) {
                     clearTimeout(node.touchTimer)
                 }
@@ -260,11 +246,11 @@ export default class MainFunctionGrid extends Component {
             onPanResponderTerminate: (evt, gestureState) => {
                 // 另一个组件已经成为了新的响应者，所以当前手势将被取消。
             },
-            onShouldBlockNativeResponder: (evt, gestureState) => {
-                // 返回一个布尔值，决定当前组件是否应该阻止原生组件成为JS响应者
-                // 默认返回true。目前暂时只支持android。
-                return true;
-            },
+            // onShouldBlockNativeResponder: (evt, gestureState) => {
+            //     // 返回一个布尔值，决定当前组件是否应该阻止原生组件成为JS响应者
+            //     // 默认返回true。目前暂时只支持android。
+            //     return true;
+            // },
         })
     }
 }
@@ -277,7 +263,7 @@ const myStyles = StyleSheet.create({
     box: {
         width: Dimensions.get('window').width / 3,
         height: Dimensions.get('window').width / 3,
-        backgroundColor: '#99ff9988',
+        backgroundColor: '#00000000',
         position: 'absolute',
     }
 })
