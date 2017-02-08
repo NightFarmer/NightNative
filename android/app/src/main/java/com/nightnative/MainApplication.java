92013c419058eef5796a2a4a5017486d00252092
NightFarmer <NightFarmer@163.com>
2017-02-08 11:05:33 +0800

    整理工程文件

--- android/app/src/main/java/com/nightnative/MainApplication.java
+++ android/app/src/main/java/com/nightnative/MainApplication.java
@@ -1,57 +1,58 @@
 package com.nightnative;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.imagepicker.ImagePickerPackage;
-import com.lwansbrough.RCTCamera.RCTCameraPackage;
-import com.fileopener.FileOpenerPackage;
-import com.reactnative.ivpusic.imagepicker.PickerPackage;
-import com.reactnativecomponent.amap.RCTAMapPackage;
+import com.reactnativecomponent.swiperefreshlayout.RCTSwipeRefreshLayoutPackage;
 import com.reactnativecomponent.amaplocation.RCTAMapLocationPackage;
+import com.reactnativecomponent.amap.RCTAMapPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
+import com.fileopener.FileOpenerPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.rnfs.RNFSPackage;
+import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.reactnativecomponent.swiperefreshlayout.RCTSwipeRefreshLayoutPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-        @Override
-        protected boolean getUseDeveloperSupport() {
-            return BuildConfig.DEBUG;
-        }
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
 
-        @Override
-        protected List<ReactPackage> getPackages() {
-            return Arrays.<ReactPackage>asList(
-                    new MainReactPackage(),
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new RCTSwipeRefreshLayoutPackage(),
+            new RCTAMapLocationPackage(),
+            new RCTAMapPackage(),
             new ImagePickerPackage(),
-            new RCTCameraPackage(),
             new PickerPackage(),
-                    new RNFSPackage(),
-                    new FileOpenerPackage(),
-                    new RCTSwipeRefreshLayoutPackage(),
-                    new RCTAMapPackage(),
-                    new RCTAMapLocationPackage(),
-                    new MyExReactPackage()
-            );
-        }
-    };
-
-    @Override
-    public ReactNativeHost getReactNativeHost() {
-        return mReactNativeHost;
+            new FileOpenerPackage(),
+            new RCTCameraPackage(),
+            new RNFSPackage()
+      );
     }
+  };
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        SoLoader.init(this, /* native exopackage */ false);
-    }
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
 }
